484b79d39219fa790da84ef71db984753ca6e266
Kobayashi Reni <ee0714n0222@gmail.com>
2022-04-20 18:45:28 +0900

    test

---
+++ chapter12/MyScheduler/app/src/main/java/com/example/username/myscheduler/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.username.myscheduler
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import io.realm.Realm
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        realm = Realm.getDefaultInstance()
+        val schedules = realm.where(Schedule::class.java).findAll()
+        listView.adapter = ScheduleAdapter(schedules)
+
+        fab.setOnClickListener { view ->
+            startActivity<ScheduleEditActivity>()
+        }
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            val schedule = parent.getItemAtPosition(position) as Schedule
+            startActivity<ScheduleEditActivity>(
+                    "schedule_id" to schedule.id )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+}
